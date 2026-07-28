db35855c6a264b6745012c6745da531b2c58a892
testorapp <testora.inc@gmail.com>
2026-07-28 14:39:04 +0300

    Delete testtbackend-main directory

--- testtbackend-main/extensions.py
+++
@@ -1,9 +0,0 @@
-"""Testora Backend - Flask Extensions"""
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail
-from flask_cors import CORS
-
-db = SQLAlchemy()
-mail = Mail()
-cors = CORS()
-
